13458138ec062f9ab6f9442fd939e2df8767d3b9
Stuart Robson <services@sturobson.com>
2023-01-02 23:44:59 +0000

    initial eleventy config started

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
+
+  return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dir: {
+      input: "site",
+      output: "dist"
+    }
+  }
+};
